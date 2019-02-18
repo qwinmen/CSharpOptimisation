11105f6404399ae7c8cd0fa24c8c43c572e83db3
Dmitry Safronov <qwinmen@ya.ru>
2019-02-18 12:41:49 +0300

    CSO добавлен OperationTimer для замеров времени выполнения задач

---
+++ Optimisation/TestAlgPower/TestAlgPower/OperationTimer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestAlgPower
+{
+	/// <summary>
+	/// Класс для оценки времени выполнения операций
+	/// </summary>
+	internal sealed class OperationTimer: IDisposable
+	{
+		private Stopwatch m_startTime;
+		private String m_text;
+		private Int32 m_collectionCount;
+
+		public OperationTimer(string text)
+		{
+			PrepareForOperation();
+
+			m_text = text;
+			m_collectionCount = GC.CollectionCount(0);
+
+			//Эта команда должна быть последней в этом методе
+			//Для максимально точной оценки быстродействия
+			m_startTime = Stopwatch.StartNew();
+		}
+
+		public void Dispose()
+		{
+			m_startTime.Stop();
+			Debug.WriteLine("► {0} (GCs={1}) {2} {3}", m_startTime.Elapsed, GC.CollectionCount(0), m_text, m_collectionCount);
+		}
+
+		private static void PrepareForOperation()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+		}
+	}
+	
+	/*
+	//пример использования в коде:
+	using(new OperationTimer("GetAll")){
+		//тут код операций для измерений
+	}
+	*/
+}
---
+++ Optimisation/TestAlgPower/TestAlgPower/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Int32 = System.Int32;
+
+namespace TestAlgPower
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ValueTypePerfTest();
+			ReferenceTypePerfTest();
+
+			Console.ReadLine();
+		}
+
+		private static void ValueTypePerfTest()
+		{
+			const int count = 100000000;
+			using (new OperationTimer("List<Int32>"))
+			{
+				List<Int32> l = new List<int>();
+				for (int i = 0; i < count; i++)
+				{
+					l.Add(i);			//без упаковки
+					int x = l[i];		//без распаковки
+				}
+
+				l = null;	//для удаления в процессе уборки мусора
+			}
+
+			using (new OperationTimer("ArrayList of Int32"))
+			{
+				ArrayList a = new ArrayList();
+				for (int i = 0; i < count; i++)
+				{
+					a.Add(i);				//Упаковка
+					int x = (Int32)a[i];	//Распаковка
+				}
+
+				a = null;	//Для удаления в процессе уборки мусора
+			}
+		}
+
+		private static void ReferenceTypePerfTest()
+		{
+			const Int32 count = 100000000;
+			using (new OperationTimer("List<String>"))
+			{
+				List<String> l = new List<string>();
+				for (int i = 0; i < count; i++)
+				{
+					l.Add("X");			//Копирование ссылки
+					string x = l[i];	//Копирование ссылки
+				}
+
+				l = null;	//Для удаления в процессе уборки мусора
+			}
+
+			using (new OperationTimer("ArrayList of String"))
+			{
+				ArrayList a = new ArrayList();
+				for (int i = 0; i < count; i++)
+				{
+					a.Add("X");					//Копирование ссылки
+					String x = (String)a[i];	//Проверка преобразования и копирование ссылки
+				}
+
+				a = null;	//Для удаления в процессе уборки мусора
+			}
+		}
+	}
+}

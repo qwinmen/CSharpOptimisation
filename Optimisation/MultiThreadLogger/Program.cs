eb0896afc5c131a48bf9fb04d4e9b4039d3687eb
Dmitry Safronov <qwinmen@ya.ru>
2020-09-24 15:43:27 +0300

    CSO пример реализаций логера для мультипоточной обработки сообщений

--- Optimisation/MultiThreadLogger/Program.cs
+++ Optimisation/MultiThreadLogger/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Threading;
 
 /*
  * Использование логера из нескольких потоков одновременно.
@@ -9,10 +11,46 @@ namespace MultiThreadLogger
 {
 	class Program
 	{
+		static MtLogger _logger = new MtLogger();
+
 		static void Main(string[] args)
 		{
-			var logger = new MtLogger();
-			Console.WriteLine("Hello World!");
+			int t = 2;
+			Thread[] ts = new Thread[t];
+			for (int i = 0; i < t; i++)
+			{
+				ts[i] = new Thread(() =>
+					{
+						int temp = i;
+						CallService(temp);
+					});
+				ts[i].Start();
+			}
+
+			for (int i = 0; i < t; i++)
+			{
+				ts[i].Join();
+			}
+
+			Debug.WriteLine("# Hello World!");
+			Console.Read();
+		}
+
+		public static void CallService(int i)
+		{
+			string msg = "# ServiceCall::" + i;
+			Debug.WriteLine(msg);
+			_logger.WriteLine(msg);
+			ReadRooms(i);
+		}
+
+		public static void ReadRooms(int i)
+		{
+			string msg = "# Reading Room::" + i;
+			Debug.WriteLine(msg);
+			_logger.WriteLine(msg);
+
+			Thread.Sleep(2000);
 		}
 	}
 }
